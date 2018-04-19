1fffc5e1b9982503348ea1815db9242402c1afe0
Eric Martineau <ericm@infusionsoft.com>
2018-04-19 11:43:31 -0700

    Ctr

--- src/main/java/liqp/tags/InitializingTag.java
+++ src/main/java/liqp/tags/InitializingTag.java
@@ -4,6 +4,10 @@ import liqp.nodes.LNode;
 
 public abstract class InitializingTag extends Tag {
 
+  protected InitializingTag(String name) {
+    super(name);
+  }
+
   /**
    * This method creates an instance tag based on the input nodes.  This allows for any optimizations or parsing to be
    * done up-front, instead of on each render iteration.
