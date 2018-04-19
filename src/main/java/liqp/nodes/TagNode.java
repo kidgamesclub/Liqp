3153b486a2d0b63e3adb25eda15c3928d6371461
Eric Martineau <ericm@infusionsoft.com>
2018-04-19 12:03:24 -0700

    Fixing instance tags

--- src/main/java/liqp/nodes/TagNode.java
+++ src/main/java/liqp/nodes/TagNode.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 @Getter
 public class TagNode implements LNode {
 
-    private Tag tag;
-    private LNode[] tokens;
+  private Tag tag;
+  private LNode[] tokens;
 
     public TagNode(Tag tag, List<LNode> tokens) {
         this(tag.name, tag, tokens.toArray(new LNode[tokens.size()]));
@@ -19,25 +19,25 @@ public class TagNode implements LNode {
         this(tag.name, tag, tokens);
     }
 
-    public TagNode(String tagName, Tag tag, LNode... tokens) {
-        if (tagName == null) {
-            throw new IllegalArgumentException("tagName == null");
-        }
-        if (tag == null) {
-            throw new IllegalArgumentException("no tag available named: " + tagName);
-        }
-        this.tag = tag;
-        this.tokens = tokens;
-
-        if (tag instanceof InitializingTag) {
-          ((InitializingTag)tag).init(tokens);
-        }
+  public TagNode(String tagName, Tag tag, LNode... tokens) {
+    if (tagName == null) {
+      throw new IllegalArgumentException("tagName == null");
+    }
+    if (tag == null) {
+      throw new IllegalArgumentException("no tag available named: " + tagName);
     }
 
-    @Override
-    public Object render(RenderContext context) {
-        return tag.render(context, tokens);
+    if (tag instanceof CustomTag) {
+      this.tag = ((CustomTag) tag).createTagForNode(tokens);
+    } else {
+      this.tag = tag;
     }
 
+    this.tokens = tokens;
+  }
 
+  @Override
+  public Object render(RenderContext context) {
+    return tag.render(context, tokens);
+  }
 }
